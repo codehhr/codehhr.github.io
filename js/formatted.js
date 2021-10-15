a5682e336f4ec4a718b2b1d1ab86d2229fc939d8
codehhr <1871973389@qq.com>
2021-10-15 21:41:23 +0800

    Site updated: 2021-10-15 21:41:23

---
+++ js/formatted.js
@@ -0,0 +1,252 @@
+for (
+  var arr1 = document.getElementsByClassName("recent-post-item"), i = 0;
+  i < arr1.length;
+  i++
+)
+  arr1[i].classList.add("wow"),
+    arr1[i].classList.add("animate__zoomIn"),
+    arr1[i].setAttribute("data-wow-duration", "700ms"),
+    arr1[i].setAttribute("data-wow-delay", "30ms"),
+    arr1[i].setAttribute("data-wow-offset", "150"),
+    arr1[i].setAttribute("data-wow-iteration", "1");
+for (
+  var arr2 = document.getElementsByClassName("card-widget"), i = 0;
+  i < arr2.length;
+  i++
+)
+  arr2[i].classList.add("wow"),
+    arr2[i].classList.add("animate__zoomIn"),
+    arr2[i].setAttribute("data-wow-duration", "700ms"),
+    arr2[i].setAttribute("data-wow-delay", "30ms"),
+    arr2[i].setAttribute("data-wow-offset", "150"),
+    arr2[i].setAttribute("data-wow-iteration", "1");
+for (
+  var arr3 = document.getElementsByClassName("flink-list-item"), i = 0;
+  i < arr3.length;
+  i++
+)
+  arr3[i].classList.add("wow"),
+    arr3[i].classList.add("animate__zoomIn"),
+    arr3[i].setAttribute("data-wow-duration", "700ms"),
+    arr3[i].setAttribute("data-wow-delay", "30ms"),
+    arr3[i].setAttribute("data-wow-offset", "150"),
+    arr3[i].setAttribute("data-wow-iteration", "1");
+for (
+  var arr4 = document.getElementsByClassName("figure.highlight"), i = 0;
+  i < arr4.length;
+  i++
+)
+  arr4[i].classList.add("wow"),
+    arr4[i].classList.add("animate__zoomIn"),
+    arr4[i].setAttribute("data-wow-duration", "700ms"),
+    arr4[i].setAttribute("data-wow-delay", "30ms"),
+    arr4[i].setAttribute("data-wow-offset", "150"),
+    arr4[i].setAttribute("data-wow-iteration", "1");
+for (
+  var arr5 = document.getElementsByClassName("link-card"), i = 0;
+  i < arr5.length;
+  i++
+)
+  arr5[i].classList.add("wow"),
+    arr5[i].classList.add("animate__zoomIn"),
+    arr5[i].setAttribute("data-wow-duration", "700ms"),
+    arr5[i].setAttribute("data-wow-delay", "30ms"),
+    arr5[i].setAttribute("data-wow-offset", "150"),
+    arr5[i].setAttribute("data-wow-iteration", "1");
+for (
+  var arr6 = document.getElementsByClassName(
+      "img.link-cardcover.entered.loaded"
+    ),
+    i = 0;
+  i < arr6.length;
+  i++
+)
+  arr6[i].classList.add("wow"),
+    arr6[i].classList.add("animate__zoomIn"),
+    arr6[i].setAttribute("data-wow-duration", "700ms"),
+    arr6[i].setAttribute("data-wow-delay", "30ms"),
+    arr6[i].setAttribute("data-wow-offset", "150"),
+    arr6[i].setAttribute("data-wow-iteration", "1");
+for (
+  var arr7 = document.getElementsByClassName(".jg-entry.jg-entry-visible"),
+    i = 0;
+  i < arr7.length;
+  i++
+)
+  arr7[i].classList.add("wow"),
+    arr7[i].classList.add("animate__zoomIn"),
+    arr7[i].setAttribute("data-wow-duration", "700ms"),
+    arr7[i].setAttribute("data-wow-delay", "30ms"),
+    arr7[i].setAttribute("data-wow-offset", "150"),
+    arr7[i].setAttribute("data-wow-iteration", "1");
+for (
+  var arr8 = document.getElementsByClassName("figure.gallery-group"), i = 0;
+  i < arr8.length;
+  i++
+)
+  arr8[i].classList.add("wow"),
+    arr8[i].classList.add("animate__zoomIn"),
+    arr8[i].setAttribute("data-wow-duration", "700ms"),
+    arr8[i].setAttribute("data-wow-delay", "30ms"),
+    arr8[i].setAttribute("data-wow-offset", "150"),
+    arr8[i].setAttribute("data-wow-iteration", "1");
+for (
+  var arr9 = document.getElementsByClassName(".hexo-douban-item"), i = 0;
+  i < arr9.length;
+  i++
+)
+  arr9[i].classList.add("wow"),
+    arr9[i].classList.add("animate__zoomIn"),
+    arr9[i].setAttribute("data-wow-duration", "700ms"),
+    arr9[i].setAttribute("data-wow-delay", "30ms"),
+    arr9[i].setAttribute("data-wow-offset", "150"),
+    arr9[i].setAttribute("data-wow-iteration", "1");
+for (
+  var arr10 = document.getElementsByClassName(".note"), i = 0;
+  i < arr10.length;
+  i++
+)
+  arr10[i].classList.add("wow"),
+    arr10[i].classList.add("animate__zoomIn"),
+    arr10[i].setAttribute("data-wow-duration", "700ms"),
+    arr10[i].setAttribute("data-wow-delay", "30ms"),
+    arr10[i].setAttribute("data-wow-offset", "150"),
+    arr10[i].setAttribute("data-wow-iteration", "1");
+for (
+  var arr11 = document.getElementsByClassName(".post-reward"), i = 0;
+  i < arr11.length;
+  i++
+)
+  arr11[i].classList.add("wow"),
+    arr11[i].classList.add("animate__zoomIn"),
+    arr11[i].setAttribute("data-wow-duration", "700ms"),
+    arr11[i].setAttribute("data-wow-delay", "30ms"),
+    arr11[i].setAttribute("data-wow-offset", "150"),
+    arr11[i].setAttribute("data-wow-iteration", "1");
+for (
+  var arr12 = document.getElementsByClassName(".wpac-rating"), i = 0;
+  i < arr12.length;
+  i++
+)
+  arr12[i].classList.add("wow"),
+    arr12[i].classList.add("animate__zoomIn"),
+    arr12[i].setAttribute("data-wow-duration", "700ms"),
+    arr12[i].setAttribute("data-wow-delay", "30ms"),
+    arr12[i].setAttribute("data-wow-offset", "150"),
+    arr12[i].setAttribute("data-wow-iteration", "1");
+for (
+  var arr13 = document.getElementsByClassName("c-player.cp-big"), i = 0;
+  i < arr13.length;
+  i++
+)
+  arr13[i].classList.add("wow"),
+    arr13[i].classList.add("animate__zoomIn"),
+    arr13[i].setAttribute("data-wow-duration", "700ms"),
+    arr13[i].setAttribute("data-wow-delay", "30ms"),
+    arr13[i].setAttribute("data-wow-offset", "150"),
+    arr13[i].setAttribute("data-wow-iteration", "1");
+for (
+  var arr14 = document.getElementsByClassName(
+      ".aplayer.aplayer-tag-marker.meting-tag-marker.aplayer-withlrc.aplayer-withlist"
+    ),
+    i = 0;
+  i < arr14.length;
+  i++
+)
+  arr14[i].classList.add("wow"),
+    arr14[i].classList.add("animate__zoomIn"),
+    arr14[i].setAttribute("data-wow-duration", "700ms"),
+    arr14[i].setAttribute("data-wow-delay", "30ms"),
+    arr14[i].setAttribute("data-wow-offset", "150"),
+    arr14[i].setAttribute("data-wow-iteration", "1");
+for (
+  var arr15 = document.getElementsByClassName(".article-sort-item"), i = 0;
+  i < arr15.length;
+  i++
+)
+  arr15[i].classList.add("wow"),
+    arr15[i].classList.add("animate__zoomIn"),
+    arr15[i].setAttribute("data-wow-duration", "700ms"),
+    arr15[i].setAttribute("data-wow-delay", "30ms"),
+    arr15[i].setAttribute("data-wow-offset", "150"),
+    arr15[i].setAttribute("data-wow-iteration", "1");
+for (
+  var arr16 = document.getElementsByClassName(".moments-item"), i = 0;
+  i < arr16.length;
+  i++
+)
+  arr16[i].classList.add("wow"),
+    arr16[i].classList.add("animate__zoomIn"),
+    arr16[i].setAttribute("data-wow-duration", "700ms"),
+    arr16[i].setAttribute("data-wow-delay", "30ms"),
+    arr16[i].setAttribute("data-wow-offset", "150"),
+    arr16[i].setAttribute("data-wow-iteration", "1");
+for (
+  var arr17 = document.getElementsByClassName(".xk-card"), i = 0;
+  i < arr17.length;
+  i++
+)
+  arr17[i].classList.add("wow"),
+    arr17[i].classList.add("animate__zoomIn"),
+    arr17[i].setAttribute("data-wow-duration", "700ms"),
+    arr17[i].setAttribute("data-wow-delay", "30ms"),
+    arr17[i].setAttribute("data-wow-offset", "150"),
+    arr17[i].setAttribute("data-wow-iteration", "1");
+for (
+  var arr18 = document.getElementsByClassName(".timenode"), i = 0;
+  i < arr18.length;
+  i++
+)
+  arr18[i].classList.add("wow"),
+    arr18[i].classList.add("animate__zoomIn"),
+    arr18[i].setAttribute("data-wow-duration", "700ms"),
+    arr18[i].setAttribute("data-wow-delay", "30ms"),
+    arr18[i].setAttribute("data-wow-offset", "150"),
+    arr18[i].setAttribute("data-wow-iteration", "1");
+for (
+  var arr19 = document.getElementsByClassName(
+      ".tk-comments-container .tk-comment"
+    ),
+    i = 0;
+  i < arr19.length;
+  i++
+)
+  arr19[i].classList.add("wow"),
+    arr19[i].classList.add("animate__zoomIn"),
+    arr19[i].setAttribute("data-wow-duration", "700ms"),
+    arr19[i].setAttribute("data-wow-delay", "30ms"),
+    arr19[i].setAttribute("data-wow-offset", "150"),
+    arr19[i].setAttribute("data-wow-iteration", "1");
+for (
+  var arr20 = document.getElementsByClassName(".charts"), i = 0;
+  i < arr20.length;
+  i++
+)
+  arr20[i].classList.add("wow"),
+    arr20[i].classList.add("animate__zoomIn"),
+    arr20[i].setAttribute("data-wow-duration", "700ms"),
+    arr20[i].setAttribute("data-wow-delay", "30ms"),
+    arr20[i].setAttribute("data-wow-offset", "150"),
+    arr20[i].setAttribute("data-wow-iteration", "1");
+for (
+  var arr21 = document.getElementsByClassName(".aside-list-item"), i = 0;
+  i < arr21.length;
+  i++
+)
+  arr21[i].classList.add("wow"),
+    arr21[i].classList.add("animate__zoomIn"),
+    arr21[i].setAttribute("data-wow-duration", "700ms"),
+    arr21[i].setAttribute("data-wow-delay", "30ms"),
+    arr21[i].setAttribute("data-wow-offset", ""),
+    arr21[i].setAttribute("data-wow-iteration", "1");
+for (
+  var arr22 = document.getElementsByClassName(".error-content"), i = 0;
+  i < arr22.length;
+  i++
+)
+  arr22[i].classList.add("wow"),
+    arr22[i].classList.add("animate__zoomIn"),
+    arr22[i].setAttribute("data-wow-duration", "700ms"),
+    arr22[i].setAttribute("data-wow-delay", "30ms"),
+    arr22[i].setAttribute("data-wow-offset", ""),
+    arr22[i].setAttribute("data-wow-iteration", "1");

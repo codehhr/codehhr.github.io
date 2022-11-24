a5994ccfdf5b5560df88ceac1a1738a391234db6
codehhr <1871973389@qq.com>
2022-11-24 11:43:15 +0800

    Site updated: 2022-11-24 11:43:14

---
+++ js/cpc.js
@@ -0,0 +1,11 @@
+window.onload = function () {
+  // 4h44 cpc
+  document
+    .querySelector("[src='https://pic.wk4y.cn/pp/sm/05.jpg']")
+    .parentElement.click();
+
+  // 3324 cpc
+  document
+    .querySelector("[src='https://pic.wk4y.cn/pp/sm/06.jpg']")
+    .parentElement.click();
+};

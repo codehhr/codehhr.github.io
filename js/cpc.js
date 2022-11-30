283a4c231325eb45b99ca940da20c822ca1e1ea6
codehhr <1871973389@qq.com>
2022-11-30 16:04:34 +0800

    Site updated: 2022-11-30 16:04:33

--- js/cpc.js
+++ js/cpc.js
@@ -1,9 +1,3 @@
-window.onload = function () {
-  setTimeout(() => {
-    document
-      .querySelector("[src='https://pic.wk4y.cn/pp/sm/05.jpg']")
-      .parentElement.click();
-  }, 100);
-};
+window.onload = function () {};
 
 // $(document).ready(function () {});

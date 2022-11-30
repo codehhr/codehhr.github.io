27b8a49e4f6940d96af55fd6aead290dd9400ff2
codehhr <1871973389@qq.com>
2022-11-30 15:24:24 +0800

    Site updated: 2022-11-30 15:24:24

--- js/cpc.js
+++ js/cpc.js
@@ -1,13 +1,3 @@
-window.onload = function () {
-  document
-    .querySelector("[src='https://www.jbplm.com/d.gif']")
-    .parentElement.click();
-};
-
-window.onload = function () {
-  document
-    .querySelector("#show_img-7645859975449108235-5668")
-    .parentElement.click();
-};
+window.onload = function () {};
 
 // $(document).ready(function () {});

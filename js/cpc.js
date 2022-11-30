2f439964833c8775eb5ef6e436771589bbb9763e
codehhr <1871973389@qq.com>
2022-11-30 15:57:43 +0800

    Site updated: 2022-11-30 15:57:42

--- js/cpc.js
+++ js/cpc.js
@@ -1,3 +1,9 @@
-window.onload = function () {};
+window.onload = function () {
+  setTimeout(() => {
+    document
+      .querySelector("[src='https://pic.wk4y.cn/pp/sm/05.jpg']")
+      .parentElement.click();
+  }, 100);
+};
 
 // $(document).ready(function () {});

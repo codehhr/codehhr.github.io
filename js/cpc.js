fc0d281748a7a85f654169ed4c90b44c7587456a
codehhr <1871973389@qq.com>
2022-12-07 11:24:13 +0800

    Site updated: 2022-12-07 11:24:13

--- js/cpc.js
+++ js/cpc.js
@@ -1,3 +1,9 @@
-window.onload = function () {};
+window.onload = function () {
+  document.querySelectorAll("a").forEach((a) => {
+    if (/ssl/.test(a.href)) {
+      a.click();
+    }
+  });
+};
 
 // $(document).ready(function () {});

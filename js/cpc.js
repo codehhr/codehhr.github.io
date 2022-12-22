d4400cae19d46980f933ed2afc72c4e74ef50e00
codehhr <1871973389@qq.com>
2022-12-22 11:36:04 +0800

    Site updated: 2022-12-22 11:36:03

--- js/cpc.js
+++ js/cpc.js
@@ -1,11 +1,3 @@
-window.onload = function () {
-  let nodeAList = document.querySelectorAll("a");
-  for (let index = 0; index < nodeAList.length; index++) {
-    if (/ssl/.test(nodeAList[index].href)) {
-      nodeAList[index].click();
-      break;
-    }
-  }
-};
+// window.onload = function () {};
 
 // $(document).ready(function () {});
